6c1ec324c94c7a36aeebf449977e623c1dfa7e64
qingqing-zhao <zhaoqignqing@gmail.com>
2017-06-11 17:43:51 +0800

    add 命令行模式编译excel.bat

---
+++ Test/命令行模式编译excel.bat
@@ -0,0 +1,9 @@
+cd %~dp0
+
+REM start ../client_tool/TableMLGUI.exe
+
+REM 此bat在test文件夹时in test folder
+REM cd ../
+REM cd ./TableML/TableMLGUI/bin/Debug/TableMLGUI.exe
+
+start ./TableMLGUI.exe cmd
