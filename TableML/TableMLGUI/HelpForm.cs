3c7f4c0deee67c07853ff9abf763a88202f42170
qingqing-zhao <zhaoqignqing@gmail.com>
2017-06-12 16:07:39 +0800

    HelpForm test script

--- TableML/TableMLGUI/HelpForm.cs
+++ TableML/TableMLGUI/HelpForm.cs
@@ -1,8 +1,13 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TableMLGUI
 {
+    /// <summary>
+    /// 如果加载慢，可以试试：https://stackoverflow.com/questions/3086063/winform-webbrowser-control-slow-to-load-best-method-for-pre-loading
+    /// </summary>
     public partial class HelpForm : Form
     {
         public HelpForm()
@@ -14,8 +19,17 @@ namespace TableMLGUI
         public void DoStart()
         {
             string helpUrl = ConfigurationManager.AppSettings.Get("HelpUrl").Trim();
+            webBrowser.Navigated += WebBrowser_Navigated;
             webBrowser.ScriptErrorsSuppressed = true;
             webBrowser.Navigate(helpUrl);
+            //            ConsoleHelper.WriteLine("nav start");
+        }
+
+        private void WebBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            //            ConsoleHelper.WriteLine("nav end");
+            //此方法不能滚动到指定位置
+            //            webBrowser.AutoScrollOffset = new Point(0, 300);
         }
     }
 }
